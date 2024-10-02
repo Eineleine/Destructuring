03df8cdca1d893ab14f3f8aee40915599d36996d
Eineleine <dsa210487@mail.ru>
2024-10-02 21:15:35 +0300

    Destructuring task is ready

---
+++ src/__test__/index.test.js
@@ -0,0 +1,40 @@
+import { secretAttack } from '../index';
+
+const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+        {
+            id: 8,
+            name: 'Двойной выстрел',
+            icon: 'http://...',
+            description: 'Двойной выстрел наносит двойной урон'
+        }, 
+        {
+            id: 9,
+            name: 'Нокаутирующий удар',
+            icon: 'http://...'
+        }
+    ]	
+}
+
+test('objects array with four fields', () => {
+    const expected = [{
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    }];
+  
+    expect(expected).toEqual(secretAttack(character));
+  });
